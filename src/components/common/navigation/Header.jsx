9c04a32506002daf8e4e7d120a1c521dee4dc2d0
justallyt <albertjunk2030@gmail.com>
2024-07-30 23:58:28 +0300

    Updates

---
+++ src/components/common/navigation/Header.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+import logo from "../../../assets/logo.png"
+import { HiMiniPhone } from "react-icons/hi2";
+
+const Header = () => {
+  return (
+    <header>
+             <div className="inner-row">
+                       <div className="header-content">
+                                  <Link to={"/"} className="logo">
+                                             <img src={logo} alt={"KKCO logo"} />
+                                   </Link>
+                                   <nav>
+                                             <ul>
+                                                     <li><Link to={"/"}>Home</Link></li>
+                                                     <li><Link to={"/about"}>About</Link></li>
+                                                     <li><Link to={"/services"}>Services</Link></li>
+                                                     <li><Link to={"/insights"}>Insights</Link></li>
+                                                     <li><Link to={"/contact"}>Contact</Link></li>
+                                             </ul>
+                                              <div className="header-action">
+                                                         <span><HiMiniPhone /></span>
+                                                         <p>Book Appointment</p>
+                                              </div>
+                                   </nav>
+                                  
+                       </div>
+             </div>
+    </header>
+  )
+}
+
+export default Header
